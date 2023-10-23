c258a60ba2794c2fd88693e3ce099cbc351406f0
Zain Ahmed <zain.ahmed199524@gmail.com>
2023-10-23 17:25:18 +0800

    update ui code

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -5,7 +5,51 @@ import ProfilPic from '../../../assets/images/profile-pic.png'
 
 function Browse(props:any) {
     return (
-        <div className='p-4'>
+        <div className=''>
+            <div className=' mb-8'>
+            <nav
+                className="relative h-14 flex w-full items-center justify-between bg-white py-2 text-neutral-600 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 dark:text-neutral-200 md:flex-wrap md:justify-start"
+                data-te-navbar-ref>
+                <div className="flex w-full flex-wrap items-center justify-between px-3">
+                <div
+                    className=" grow basis-[100%] items-center lg:!flex lg:basis-auto"
+                    id="navbarSupportedContentY"
+                    data-te-collapse-item>
+                    <ul
+                    className="mr-auto flex flex-row lg:flex-row items-center"
+                    data-te-navbar-nav-ref>
+                    <li className=" px-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                        <a
+                        className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+                        href="#!"
+                        data-te-nav-link-ref
+                        data-te-ripple-init
+                        data-te-ripple-color="light"
+                        >Home</a>
+                    </li>
+                    <li className="px-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                        <a
+                        className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+                        href="#!"
+                        data-te-nav-link-ref
+                        data-te-ripple-init
+                        data-te-ripple-color="light"
+                        >Profile</a>
+                    </li>
+                    <li className=" px-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                        <a
+                        className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+                        href="#!"
+                        data-te-nav-link-ref
+                        data-te-ripple-init
+                        data-te-ripple-color="light"
+                        >Settings</a>
+                    </li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
+            </div>
             <div className='flex flex-col justify-center'>
                 <div className='px-4 flex flex-row'>
                     <input className='w-full h-10 rounded-full placeholder:px-2 px-2 mx-2' placeholder='Search...' />
